da78e9473a1e944d1b579919cf10de3a2ab4e565
James Grady <jaymealann@gmail.com>
2023-10-04 16:30:21 -0400

    Update BitwiseOperations.cpp
    Updates to old code

--- testing/BitwiseOperations.cpp
+++ testing/BitwiseOperations.cpp
@@ -5,54 +5,47 @@
 #include <iostream>
 
 bool isbitset(uint32_t bitfield, uint32_t bitIndex) {
-    if (bitfield & (1 << (bitIndex - 1))) {
-        return true;
-    }
-    return false;
+    return (bitfield & (1U << (bitIndex - 1))) != 0;
 }
 
 void setBit(uint32_t& bitfield, uint32_t bitindex) {
-    bitfield |= 1UL << bitindex-1;
+    bitfield |= 1U << (bitindex - 1);
 }
 
-void clearbit(uint32_t& bitfield, uint32_t bitindex) {
-    bitfield &= ~(1UL << bitindex -1);
+void clearBit(uint32_t& bitfield, uint32_t bitindex) {
+    bitfield &= ~(1U << (bitindex - 1));
 }
 
-void flipbit(uint32_t& bitfield, uint32_t bitindex) {
-    bitfield ^= 1UL << bitindex -1;
+void flipBit(uint32_t& bitfield, uint32_t bitindex) {
+    bitfield ^= 1U << (bitindex - 1);
 }
 
-void EqualByBitmask(unsigned int bitfield1, unsigned int bitfield2, unsigned int bitmask) {
+void equalByBitmask(uint32_t bitfield1, uint32_t bitfield2, uint32_t bitmask) {
     int mask1 = (bitfield1 & bitmask);
     int mask2 = (bitfield2 & bitmask);
     if (mask1 == mask2) {
-        std::cout << "TRUE:  " << std::endl;
-    }
-    else {
-        std::cout << mask2 << std::endl;
+        std::cout << "TRUE" << std::endl;
+    } else {
+        std::cout << "FALSE" << std::endl;
     }
 }
 
-int main()
-{
-    EqualByBitmask(85, 255, 80);
+int main() {
+    equalByBitmask(85, 255, 80);
 
     // Will return 0 for false and 1 for true
-    unsigned int test = 80;
-    std::cout << "\n ~BITWISE FUNCTION TESTING~";
-    std::cout << "\n Test1: SetBit: " << isbitset(test, 2);
+    uint32_t test = 80;
+    std::cout << "\n~BITWISE FUNCTION TESTING~";
+    std::cout << "\nTest1: SetBit: " << isbitset(test, 2);
     setBit(test, 2);
-    std::cout << "\n Test1: SetBit: " << isbitset(test, 2);
-    std::cout << "\n Test2: SetBit: " << isbitset(test, 1);
+    std::cout << "\nTest1: SetBit: " << isbitset(test, 2);
+    std::cout << "\nTest2: SetBit: " << isbitset(test, 1);
     setBit(test, 1);
-    std::cout << "\n Test2: SetBit: " << isbitset(test, 1);
-    std::cout << "\n Test3: ClearBit: " << isbitset(test, 1);
-    clearbit(test, 1);
-    std::cout << "\n Test3: ClearBit: " << isbitset(test, 1);
+    std::cout << "\nTest2: SetBit: " << isbitset(test, 1);
+    std::cout << "\nTest3: ClearBit: " << isbitset(test, 1);
+    clearBit(test, 1);
+    std::cout << "\nTest3: ClearBit: " << isbitset(test, 1);
 
-
-
-
+    return 0;
 }
 
